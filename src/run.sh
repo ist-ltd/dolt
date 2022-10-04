1af6fb32b78c7b8d5a6a37585a7618817928762b
Cameron Harris <cameron.harris@collidr.com>
2022-10-04 20:53:29 +0100

    fix: implement script that copies keys at container start

---
+++ src/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir -p /etc/ssh/keys
+cp /mnt/keys/authorized_keys /etc/ssh/keys/authorized_keys
+cp /mnt/keys/ssh_host_rsa_key /etc/ssh/keys/ssh_host_rsa_key
+
+chmod 755 /etc/ssh/keys
+chmod 644 /etc/ssh/keys/authorized_keys
+chmod 600 /etc/ssh/keys/ssh_host_rsa_key
+
+/usr/sbin/sshd -De
